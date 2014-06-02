5a4a925b4aaffa8c46e9d0dd9e83703059452262
Anderson Deluiz <AndDeluiz@users.noreply.github.com>
2014-06-02 14:47:31 -0300

    Update getConf.sh

--- getConf.sh
+++ getConf.sh
@@ -1,11 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 ################################################################################
-# @author      Anderson Deluiz (anderson.deluiz at gmail dot com)              #
+# @author      Anderson Deluiz (Twitter: @AndDeluiz)                           #
 # @name        getConf.sh                                                      #
 # @version     0.3                                                             #
+# @date        June 2nd, 2014                                                  #
 # @description Gets various configurations from running system                 #
-#
+# @usage       getConf.sh                                                      #
 ################################################################################
+
+################################################################################
+# set -x
+# set -v
+# set -n
+################################################################################
+
+#---------------------------   GLOBAL VARIABLES   -----------------------------#
+vOSName=$(uname -s)
+#------------------------------------------------------------------------------#
+
+################################################################################
+# 
+################################################################################
+usage()
+{
+   :
+}
 
 getConfLinux()
 {
@@ -62,6 +81,82 @@ getConfLinux()
             ;;
          'group')
             cat /etc/group
+            ;;
+         'limits')
+            cat /etc/security/limits.conf
+            ;;
+         'network')
+            ifconfig -a
+            ;;
+         'dns')
+            cat /etc/resolv.conf
+            ;;
+         'ntp')
+            chkconfig --list ntpd
+            ntpq -p
+            echo "******************** /etc/ntp.conf *******************"
+            grep -v '^#' /etc/ntp.conf
+            ;;
+         'services')
+            chkconfig --list 
+            echo "******************** Running Network Services *******************"
+            netstat -lpn
+            ;;
+      esac
+   done
+}
+
+getconfAIX()
+{
+   for vSection in system dump swap mpio pvs vgs lvs df passwd group limits network dns ntp services
+   do
+      echo "========================================================================"
+      echo "SECTION: ${vSection}"
+      echo "------------------------------------------------------------------------"
+      case ${vSection} in
+         'system')
+            if [ $(uname -L) != "-1 NULL" ]
+            then
+               # this is a LPAR system
+               lpartstat -i
+            fi
+            echo "\n------------------------------------------------------------------------\n"
+            prtconf
+            echo "\n------------------------------------------------------------------------\n"
+            prtconf -v
+            echo "\n------------------------------------------------------------------------\n"
+            lsattr -El sys0
+            ;;
+         'dump')
+            sysdumpdev -l
+            ;;
+         'swap')
+            lsps -a
+            ;;
+         'mpio')
+            lspath
+            ;;
+         'pvs')
+            lspv
+            ;;
+         'vgs')
+            echo "\n------------------------------------------------------------------------\n"
+            echo "Defined VGs in system"
+            echo "\n------------------------------------------------------------------------\n"
+            lsvg
+            echo "\n------------------------------------------------------------------------\n"
+            echo "Varied on (Online) VGs in system"
+            echo "\n------------------------------------------------------------------------\n"
+            lsvg -o
+            ;;
+         'lvs')
+            echo "\n------------------------------------------------------------------------\n"
+            echo "Defined Logical Volumes (LV) in system (Online VGs only)"
+            echo "\n------------------------------------------------------------------------\n"
+            lsvg -o | lsvg -i -l 
+            ;;
+         'df')
+            df -gI
             ;;
          'limits')
             cat /etc/security/limits.conf
