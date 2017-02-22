0dd96e79fc622cf07ba90147978e9c25080618cd
Anderson Deluiz <AndDeluiz@users.noreply.github.com>
2017-02-22 10:46:09 +0000

    Update testNetPerf.sh
    Add ping and traceroute to testing

--- Common/testNetPerf.sh
+++ Common/testNetPerf.sh
@@ -16,12 +16,20 @@ echo "Executado em: $(date)"
 echo "Servidor: $myName"
 echo "Aplicativo utilizado para testes: $(iperf3 --version | grep iperf)"
 echo "Server Side Command: iperf3 -i 0 -D -s"
-echo "Client Side Command: iperf3 -i 0 -w 4M -t 10 -c <iperf3_server>"
+echo "Client Side Command: iperf3 -i 0 -w 4M -t 10 --get-server-output -c <iperf3_server>"
+echo "Comandos adicionais:"
+echo "   ping -c 10 -s 504 <iperf3_server>"
+echo "   traceroute -n <iperf3_server>"
 echo "--------------------------------------------------------------------------------"
+
 
 while read SRV
 do
-   echo "$(date "+%Y-%m-%d %H:%M:%S") INFO: Testando servidor $SRV"
-   iperf3 -i 0 -w 4M -t 10 -c $SRV
+   echo -e "\n$(date "+%Y-%m-%d %H:%M:%S") INFO: Testando servidor ${SRV}\n"
+   ping -c 10 -p 504 ${SRV}
+   echo "********************"
+   traceroute -n ${SRV}
+   echo "********************"
+   iperf3 -i 0 -w 4M -t 10 --get-server-output -c ${SRV}
    echo "--------------------------------------------------------------------------------"
 done < ${inputServerList}
